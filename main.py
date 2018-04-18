472749c2c8aba27e1f841e9a623c8832cd7dea29
mcheng3 <michael.cheng65@gmail.com>
2018-04-17 22:47:54 -0400

    Finish

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 edges = []
 polygons = []
-
+transform = new_matrix()
+ident(transform)
 cstack=[]
-cstack.append(ident(new_matrix()))
+cstack.append(transform)
 
 parse_file('script', edges, polygons, cstack, screen, color )
